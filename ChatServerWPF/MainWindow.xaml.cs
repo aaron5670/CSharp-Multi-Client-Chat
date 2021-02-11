87ad49e70572e7a5d91608cbc6f71632a58b444e
Aaron van den Berg <a.vdberg98@gmail.com>
2021-02-11 11:08:38 +0100

    Initial commit

---
+++ ChatServerWPF/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _03_ChatServerWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // Stap 3:
+        TcpClient tcpClient;
+        NetworkStream networkStream;
+        Thread thread;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        // Stap 5:
+        private void AddMessage(string message)
+        {
+            this.Dispatcher.Invoke(() => listChats.Items.Add(message));
+        }
+
+        // Stap 7:
+        private void ReceiveData()
+        {
+            int bufferSize = 1024;            
+            string message = "";
+            byte[] buffer = new byte[bufferSize];
+
+            networkStream = tcpClient.GetStream();
+            
+            AddMessage("Connected!");
+
+            while (true)
+            {
+                int readBytes = networkStream.Read(buffer, 0, bufferSize);
+                message = Encoding.ASCII.GetString(buffer, 0, readBytes);
+
+                if (message == "bye")
+                    break;
+
+                AddMessage(message);
+            }
+
+            // Verstuur een reactie naar de client (afsluitend bericht)
+            buffer = Encoding.ASCII.GetBytes("bye");
+            networkStream.Write(buffer, 0, buffer.Length);
+
+            // cleanup:
+            networkStream.Close();
+            tcpClient.Close();
+
+            AddMessage("Connection closed");
+        }
+
+        private void btnStartStop_Click(object sender, RoutedEventArgs e)
+        {
+            TcpListener tcpListener = new TcpListener(IPAddress.Any, 9000);
+            tcpListener.Start();
+
+            AddMessage("Listening for client.");
+
+            tcpClient = tcpListener.AcceptTcpClient();
+            thread = new Thread(new ThreadStart(ReceiveData));
+            thread.Start();
+        }
+
+        private void btnSend_Click(object sender, RoutedEventArgs e)
+        {
+            string message = txtMessage.Text;
+
+            byte[] buffer = Encoding.ASCII.GetBytes(message);
+            networkStream.Write(buffer, 0, buffer.Length);
+
+            AddMessage(message);
+            txtMessage.Clear();
+            txtMessage.Focus();
+        }
+    }
+}
